18bc401043257566d853b8fd8a054f262284f509
Ayman Mohammed <ayman-2015@outlook.com>
2019-07-30 19:24:17 +0800

    Add project files.

---
+++ uRobot/Services/BluetoothService.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace uRobot.Services
+{
+    public class BluetoothService
+    {
+        private SerialPort _serialPort;
+
+        public bool IsConnected { get; private set; }
+
+        public BluetoothService()
+        {
+            _serialPort = new SerialPort();
+        }
+
+        public async Task ConnectAsync(string portName, int baudRate = 9600)
+        {
+            await Task.Run(() =>
+            {
+                _serialPort.PortName = portName;
+                _serialPort.BaudRate = baudRate;
+                try
+                {
+                    if (!_serialPort.IsOpen)
+                    {
+                        _serialPort.Open();
+                        IsConnected = _serialPort.IsOpen;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+
+            });
+        }
+
+        public void Disconnect()
+        {
+            try
+            {
+                if (_serialPort.IsOpen)
+                {
+                    _serialPort.Close();
+                    _serialPort.Dispose();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        public async Task SendAsync(string data)
+        {
+            await Task.Run(() =>
+            {
+                try
+                {
+                    if (_serialPort.IsOpen)
+                    {
+                        _serialPort.WriteLine(data);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+
+            });
+        }
+
+        public IEnumerable<string> GetDevices()
+        {
+            return SerialPort.GetPortNames();
+        }
+    }
+
+}
---
+++ uRobot/Views/MainView.xaml.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using uRobot.Services;
+
+namespace uRobot.Views
+{
+    /// <summary>
+    /// Interaction logic for MainView.xaml
+    /// </summary>
+    public partial class MainView : Window
+    {
+
+        private BluetoothService _bluetoothService;
+        public MainView()
+        {
+            InitializeComponent();
+            Init();
+        }
+
+        private void Init()
+        {
+            _bluetoothService = new BluetoothService();
+            devicesComBox.ItemsSource = _bluetoothService.GetDevices();
+
+            aBtn.Click += async (s, e) => await DoAsync(Actions.A);
+            bBtn.Click += async (s, e) => await DoAsync(Actions.B);
+            cBtn.Click += async (s, e) => await DoAsync(Actions.C);
+            dBtn.Click += async (s, e) => await DoAsync(Actions.D);
+
+            startBtn.Click += async (s, e) => await DoAsync(Actions.Start);
+            selectBtn.Click += async (s, e) => await DoAsync(Actions.Select);
+
+            upBtn.Click += async (s, e) => await MoveAsync(Directions.Up);
+            downBtn.Click += async (s, e) => await MoveAsync(Directions.Down);
+            leftBtn.Click += async (s, e) => await MoveAsync(Directions.Left);
+            rightBtn.Click += async (s, e) => await MoveAsync(Directions.Right);
+
+            connectBtn.Click += async (s, e) => 
+            {
+                var port = devicesComBox.SelectedValue as string;
+                if (port != null)
+                {
+                    if (!_bluetoothService.IsConnected)
+                    {
+                        connectBtn.IsEnabled = false;
+                        connectBtn.Content = "Disconnect";
+                        await _bluetoothService.ConnectAsync(port);
+                        connectBtn.IsEnabled = true;
+                    }
+                    else
+                    {
+                        connectBtn.IsEnabled = false;
+                        connectBtn.Content = "Connect";
+                         _bluetoothService.Disconnect();
+                        connectBtn.IsEnabled = true;
+                    }
+                }
+            };
+
+            refreshBtn.Click += (s, e) => 
+            {
+                devicesComBox.ItemsSource =_bluetoothService.GetDevices();
+            };
+        }
+
+        protected async override void OnKeyDown(KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.Left:
+                    await MoveAsync(Directions.Left);
+                    break;
+                case Key.Up:
+                    await MoveAsync(Directions.Up);
+                    break;
+                case Key.Right:
+                    await MoveAsync(Directions.Right);
+                    break;
+                case Key.Down:
+                    await MoveAsync(Directions.Down);
+                    break;
+                case Key.A:
+                    await DoAsync(Actions.D);
+                    break;
+                case Key.D:
+                    await DoAsync(Actions.B);
+                    break;
+                case Key.S:
+                    await DoAsync(Actions.C);
+                    break;
+                case Key.W:
+                    await DoAsync(Actions.A);
+                    break;
+                case Key.Q:
+                    await DoAsync(Actions.Select);
+                    break;
+                case Key.Space:
+                    await DoAsync(Actions.Start);
+                    break;
+            }
+            base.OnKeyDown(e);
+        }
+        protected async override void OnKeyUp(KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.Left:
+                case Key.Up:
+                case Key.Right:
+                case Key.Down:
+                    await MoveAsync(Directions.Stop);
+                    break;
+            }
+
+            base.OnKeyUp(e);
+        }
+
+        private async Task MoveAsync(Directions directions)
+        {
+            switch (directions)
+            {
+                case Directions.Up:
+                    await _bluetoothService.SendAsync("1");
+                    break;
+                case Directions.Down:
+                    await _bluetoothService.SendAsync("2");
+                    break;
+                case Directions.Right:
+                    await _bluetoothService.SendAsync("3");
+                    break;
+                case Directions.Left:
+                    await _bluetoothService.SendAsync("4");
+                    break;
+                case Directions.Stop:
+                    await _bluetoothService.SendAsync("5");
+                    break;
+            }
+        }
+        private async Task DoAsync(Actions actions)
+        {
+            switch (actions)
+            {
+                case Actions.A:
+                    await _bluetoothService.SendAsync("A");
+                    break;
+                case Actions.B:
+                    await _bluetoothService.SendAsync("B");
+                    break;
+                case Actions.C:
+                    await _bluetoothService.SendAsync("C");
+                    break;
+                case Actions.D:
+                    await _bluetoothService.SendAsync("D");
+                    break;
+                case Actions.Start:
+                    await _bluetoothService.SendAsync("S1");
+                    break;
+                case Actions.Select:
+                    await _bluetoothService.SendAsync("S2");
+                    break;
+            }
+        }
+
+    }
+
+
+    public enum Directions
+    {
+        Up,
+        Down,
+        Right,
+        Left,
+        Stop
+    }
+
+    public enum Actions
+    {
+        A,
+        B,
+        C,
+        D,
+        Start,
+        Select
+    }
+
+}
